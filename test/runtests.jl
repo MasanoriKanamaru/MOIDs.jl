8dd744252bb0f32a81adf514b56b38aeabd91818
kanamaru-masanori <kanamaru-masanori@hotmail.co.jp>
2024-12-23 17:00:56 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using MOIDs
 using Test
 
 @testset "MOIDs.jl" begin
-    # Write your tests here.
+
+    ceres = [2.7691652, 0.0760091,  73.59764,  80.30553,10.59407]
+    urania = [2.3655722, 0.127581 ,  87.42605, 307.46872, 2.09575]
+    
+    @test moid(ceres...,urania...) ≈ 0.24521440655831864
 end
